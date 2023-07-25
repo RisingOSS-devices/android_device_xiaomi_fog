9e69b877176951a7a209cf9637a4655da1df32f6
zmafnan <iamzafn22@gmail.com>
2023-07-24 19:44:00 -0500

    fog: props: Better RAM Management

--- device.mk
+++ device.mk
@@ -493,6 +493,18 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.se.type=HCE,UICC \
     sys.vendor.shutdown.waittime=500
 
+# RAM
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
