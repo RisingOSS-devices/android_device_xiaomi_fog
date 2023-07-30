e7a1af0c2f7829da5dd4ec781a6dbd568d7189ca
Notkeret <jerespn@gmail.com>
2023-07-30 14:45:51 -0400

    fog: Add vendorsetup.sh
    - Clone and sync required dependencies
    
    Signed-off-by: Notkeret <jerespn@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,28 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+echo -e "${blue}Sync required dependencies${blue}"
+#sync Hardware tree
+rm -rf hardware/xiaomi
+git clone https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
+
+#Sync to vendor qcom opensource interfaces
+rm -rf vendor/qcom/opensource/interfaces
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
+
+#Sync to vendor qcom opensource fm-commonsys
+rm -rf vendor/qcom/opensource/fm-commonsys
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_fm-commonsys.git vendor/qcom/opensource/fm-commonsys
+
+#Sync to vendor qcom data-ipa-cfg-mgr-legacy-um
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um && git clone https://github.com/PixelExperience/vendor_qcom_opensource_data-ipa-cfg-mgr-legacy-um.git vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um
+
+#sync pe settings
+rm -rf packages/resources/devicesettings && git clone https://github.com/parixxshit/android_packages_resources_devicesettings.git packages/resources/devicesettings
+
+echo -e "${color}Finished sync!${end}"
