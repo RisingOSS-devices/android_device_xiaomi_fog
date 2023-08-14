6ba33836cb5dbe91f3967f155f88d71624fd146e
Notkerd69 <jerespn@gmail.com>
2023-08-14 21:29:51 +0530

    fog: vendorsetup.sh: Add some dependencies
    - Add 'vendor_qcom_opensource_audio'
    - Add miui camera repos too
    
    Change-Id: I5f96332fea5edf89d04a1c35bcc42f7cf9c6de1f
    Signed-off-by: Notkerd69 <jerespn@gmail.com>

--- device.mk
+++ device.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
 
 # Camera
-$(call inherit-product-if-exists, device/xiaomi/fog-miuicamera/config.mk)
+$(call inherit-product-if-exists, device/xiaomi/sm6225-common-miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,9 +20,17 @@ rm -rf vendor/qcom/opensource/fm-commonsys
 git clone https://github.com/PixelExperience/vendor_qcom_opensource_fm-commonsys.git vendor/qcom/opensource/fm-commonsys
 
 #Sync to vendor qcom data-ipa-cfg-mgr-legacy-um
-rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um && git clone https://github.com/PixelExperience/vendor_qcom_opensource_data-ipa-cfg-mgr-legacy-um.git vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_data-ipa-cfg-mgr-legacy-um.git vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um
 
 #sync pe settings
-rm -rf packages/resources/devicesettings && git clone https://github.com/parixxshit/android_packages_resources_devicesettings.git packages/resources/devicesettings
+rm -rf packages/resources/devicesettings
+git clone https://github.com/parixxshit/android_packages_resources_devicesettings.git packages/resources/devicesettings
 
+#sync vendor_qcom_opensource_audio
+git clone https://github.com/PixelExperience/vendor_qcom_opensource_audio.git vendor/qcom/opensource/audio
+
+#sync miui-camera
+git clone https://github.com/PixelExperience-Devices/device_xiaomi_sm6225-common-miuicamera.git device/xiaomi/sm6225-common-miuicamera
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common-miuicamera.git vendor/xiaomi/sm6225-common-miuicamera
 echo -e "${color}Finished sync!${end}"
